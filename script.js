8b150a0be70719e26ecfdc2015444b4d05789f8c
Martin Sopko <martin.sopko@student.tuke.sk>
2020-06-06 12:48:19 +0200

    add dialog window for making wires

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
+var components=[];
+var wire=[];
 
+class Component {
+    constructor(type,value,id) {
+        this.type=type;
+        this.value=value;
+        this.id=id;
+    }
 
+    info(){
+        switch (this.type) {
+            case "resistor": return "R"+this.id;
+            case "source": return "S"+this.id;
+            case "ground":return "G"+this.id;
+            default:return this.id;
+        }
+    }
+    
+}
+
+function addWire() {
+    var dialog=document.getElementById("wireDialog");
+    var span = document.getElementsByClassName("close")[0];
+    dialog.style.display="block";
+    span.onclick = function() {
+        dialog.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == dialog) {
+            dialog.style.display = "none";
+        }
+    }
+    var select1=document.getElementById("from");
+    var select2=document.getElementById("to");
+
+    for(var i = 0; i < components.length; i++) {
+        var opt = components[i].info();
+        var id=components[i].id;
+        var el1= document.createElement("option"),el2 = document.createElement("option");
+
+        el1.textContent = opt;
+        el2.textContent = opt;
+        el1.value = id;
+        el2.value = id;
+
+        select1.appendChild(el1);
+        select2.appendChild(el2);
+
+    }
+
+}
 
 function addComponent(componentType,value) {
     var img="img/" +componentType+ value +".svg";
+    var componentId=components.length;
 
 
+    $("#simDesk").append($('<img>',{id:componentId, class:componentType, src:img,draggable:true }));
 
-    $("#simDesk").append($('<img>',{ class:componentType, src:img,draggable:true }));
+    components.push(new Component(componentType,value,componentId));
 
 
 }
