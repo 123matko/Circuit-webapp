5cfec2c2edf545829cf831a31eb64ad13c3b8032
Martin Sopko <martin.sopko@student.tuke.sk>
2020-06-13 12:11:56 +0200

    Add simulate function

--- script.js
+++ script.js
@@ -36,8 +36,13 @@ function createLine(x1, y1, x2, y2) {
 
 function drawLines() {
     var lines=document.getElementsByClassName("line");
+    var listElement=document.getElementsByClassName("listElement");
+    if(lines.length<=0){
+        return;
+    }
     for (var i=0;i<lines.length;i++){
         document.getElementById("simDesk").replaceChild(wires[i].wireLine(),lines[i]);
+        document.getElementById("list").replaceChild(wires[i].listElement(),listElement[i]);
     }
 
 }
@@ -50,16 +55,24 @@ class Wire {
         var list=document.getElementById("list");
         this.from=from;
         this.to=to;
-        var li=document.createElement("LI");
-        var text = document.createTextNode(this.info());
+
+
         this.id=id;
         let el=document.createElement("option");
         el.textContent=this.info();
         el.value=this.id;
-        li.appendChild(text);
-        list.appendChild(li);
+        el.setAttribute("class","optionElement");
+        list.appendChild(this.listElement());
         document.getElementById("simDesk").appendChild(this.wireLine());
         wiress.appendChild(el);
+    }
+
+    listElement(){
+        var text = document.createTextNode(this.info());
+        var li=document.createElement("LI");
+        li.appendChild(text);
+        li.setAttribute("class","listElement");
+        return li;
     }
 
     info(){
@@ -100,21 +113,28 @@ class Component {
     constructor(type,value,id) {
         var select1=document.getElementById("from");
         var select2=document.getElementById("to");
+        var select3=document.getElementById("componentList");
         this.type=type;
         this.value=value;
         this.id=id;
 
         var opt = this.info();
 
-        var el1= document.createElement("option"),el2 = document.createElement("option");
+        var el1= document.createElement("option"),el2 = document.createElement("option"),el3 = document.createElement("option");
 
         el1.textContent = opt;
         el2.textContent = opt;
+        el3.textContent = opt;
         el1.value = id;
         el2.value = id;
+        el3.value = id;
+        el1.setAttribute("class","selE1");
+        el2.setAttribute("class","selE2");
+        el3.setAttribute("class","selE3");
 
         select1.appendChild(el1);
         select2.appendChild(el2);
+        select3.appendChild(el3)
     }
 
     info(){
@@ -124,6 +144,9 @@ class Component {
             case "ground":return "G"+this.id;
             default:return this.id;
         }
+    }
+    getValue(){
+        return this.value;
     }
     
 }
@@ -135,10 +158,31 @@ function addWire() {
 }
 
 function removeWire() {
+    var lines=document.getElementsByClassName("line");
+    var listElement=document.getElementsByClassName("listElement");
+    var options=document.getElementsByClassName("optionElement");
     var wire = document.getElementById("wires").value;
-    wires.slice(wire,1);
-    document.getElementById("simDesk").removeChild(wires[wire].id)
-    var list=document.getElementById("list");
+    wire=Number(wire);
+    wires.splice(wire,1);
+    document.getElementById("simDesk").removeChild(lines[wire]);
+    document.getElementById("list").removeChild(listElement[wire]);
+    document.getElementById("wires").removeChild(options[wire]);
+
+
+}
+function removeComponent() {
+    var elem1=document.getElementsByClassName("selE1");
+    var elem2=document.getElementsByClassName("selE2");
+    var elem3=document.getElementsByClassName("selE3");
+    var comps=document.getElementsByClassName("compObj");
+    var com=document.getElementById("componentList").value;
+    com=Number(com);
+    components.splice(com,1);
+    document.getElementById("simDesk").removeChild(comps[com]);
+    document.getElementById("from").removeChild(elem1[com]);
+    document.getElementById("to").removeChild(elem2[com]);
+   document.getElementById("componentList").removeChild(elem3[com]);
+
 
 }
 
@@ -155,12 +199,33 @@ function openDialog(id) {
 
 }
 
+function simulate() {
+    var sources=document.getElementsByClassName("source");
+    var resistors=document.getElementsByClassName("resistor");
+    var r=0;
+    var u=0;
+
+    for(var i=0;i<sources.length;i++){
+        u+=Number(components[sources[i].id].getValue());
+    }
+    u=Number(u);
+    document.getElementById("U").innerText=u+" V";
+    for(var j=0;j<resistors.length;j++){
+        r+=Number(components[resistors[j].id].getValue());
+    }
+    r=Number(r);
+    document.getElementById("R").innerText=r+" Ω";
+    var I=u/r;
+    document.getElementById("I").innerText=parseFloat(I).toFixed(2)+" A";
+}
+
 function addComponent(componentType,value) {
     var img="img/" +componentType+ value +".svg";
     var componentId=components.length;
+    var classs=" compObj";
 
 
-    $("#simDesk").append($('<img>',{id:componentId, class:componentType, src:img,draggable:true }));
+    $("#simDesk").append($('<img>',{id:componentId, class:componentType+classs, src:img,draggable:true }));
 
     components.push(new Component(componentType,value,componentId));
 
